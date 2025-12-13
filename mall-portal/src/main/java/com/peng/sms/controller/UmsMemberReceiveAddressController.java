4a1803035c4f86ec1ed517e07434441156bd353b
layli43 <layliuanyu@gmail.com>
2025-12-13 23:26:16 +0000

    [add]:member address api.

---
+++ mall-portal/src/main/java/com/peng/sms/controller/UmsMemberReceiveAddressController.java
@@ -0,0 +1,74 @@
+package com.peng.sms.controller;
+
+import com.peng.sms.api.CommonResult;
+import com.peng.sms.model.UmsMemberReceiveAddress;
+import com.peng.sms.service.UmsMemberReceiveAddressService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Member Shipping Address Management Controller
+ */
+@Controller
+@Tag(name = "UmsMemberReceiveAddressController", description = "Member Shipping Address Management")
+@RequestMapping("/member/address")
+public class UmsMemberReceiveAddressController {
+
+    @Autowired
+    private UmsMemberReceiveAddressService memberReceiveAddressService;
+
+    @Operation(summary = "Add shipping address")
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult add(@RequestBody UmsMemberReceiveAddress address) {
+        int count = memberReceiveAddressService.add(address);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @Operation(summary = "Delete shipping address")
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult delete(@PathVariable Long id) {
+        int count = memberReceiveAddressService.delete(id);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @Operation(summary = "Update shipping address")
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult update(@PathVariable Long id, @RequestBody UmsMemberReceiveAddress address) {
+        int count = memberReceiveAddressService.update(id, address);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @Operation(summary = "List all shipping addresses")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<List<UmsMemberReceiveAddress>> list() {
+        List<UmsMemberReceiveAddress> addressList = memberReceiveAddressService.list();
+        return CommonResult.success(addressList);
+    }
+
+    @Operation(summary = "Get shipping address details")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<UmsMemberReceiveAddress> getItem(@PathVariable Long id) {
+        UmsMemberReceiveAddress address = memberReceiveAddressService.getItem(id);
+        return CommonResult.success(address);
+    }
+}
